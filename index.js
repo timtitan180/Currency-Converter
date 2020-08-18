e93719810813c4642ebd2db37526c53797557215
timtitan180 <timtudosa6@gmail.com>
2020-08-17 22:20:14 -0500

    Server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+port = 1255;
+
+app.use('/',express.static('public'));
+
+app.get('/',function(request,response){
+    response.send();
+});
+
+app.listen(port,function(err){
+    if(err) {
+        console.log(err);
+    }
+    console.log(`Server running on port ${port}`);
+})
