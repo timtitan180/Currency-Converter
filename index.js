821e8f5ff7c855bf55f314423b37812288bca6e1
Timothy Tudosa <timtudosa6@gmail.com>
2020-12-17 06:38:51 -0600

    updated server

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 port = process.env.PORT || 1255;
 
 app.use(express.static('public'));
 
 app.get('/',function(request,response){
     response.sendFile(__dirname + '/converter.html');
+   
 });
 
 app.listen(port,function(err){
     if(err) {
         console.log(err);
     }
-    console.log(`Server running on port ${port}`);
-})
+    console.log(`Server running on port ${port}`)
+});
