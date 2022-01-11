f0a9ee7c5b6cad612fde550cd55bba59a837ce4e
Tim Tudosa <timtudosa6@gmail.com>
2022-01-11 11:13:49 -0600

    Updated get method to successfully serve static files to the browser

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+
 port = process.env.PORT || 1255;
 
 app.use(express.static('public'));
 
-app.get('/',function(request,response){
-    response.sendFile(__dirname + '/converter.html');
-   
-});
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,"public/converter.html"));
+}); 
 
 app.listen(port,function(err){
     if(err) {
         console.log(err);
     }
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 });
